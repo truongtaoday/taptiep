aeb65d35af628292aff56beb251afc8fae739785
truongtaoday <80606644+truongtaoday@users.noreply.github.com>
2024-02-23 10:51:53 +0700

    NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -13,9 +13,7 @@ ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels "
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/tunnels/agents "
 echo User: administrator
 echo Pass: Linhsang97
-echo View more: https://youtube.com/c/tinhocthuchanh
-echo Visit Create VPS-RDP Part1: https://youtu.be/VnEJv49oKR8 , Part2: https://youtu.be/em221J1k520
 ping -n 10 127.0.0.1 >nul
